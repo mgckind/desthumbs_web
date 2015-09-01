b9a7c4c42d7ba367b5777359b895c0410e529579
root <mgckind@gmail.com>
2015-09-01 07:52:35 +0000

    more

--- app/readfile.py
+++ app/readfile.py
@@ -56,7 +56,9 @@ class FileHandler(BaseHandler):
             os.system("convert %s %s.png" % (f,f))
             titles.append(title)
             pngfiles.append(mypath+title+'.tif.png')
-        
+       
+
+        os.system("tar -zcf "+user_folder+"all.tar.gz "+user_folder+"*") 
         if os.path.exists(user_folder+"list.json"): os.remove(user_folder+"list.json")
         with open(user_folder+"list.json","w") as outfile:
             json.dump([dict(name=pngfiles[i],title=titles[i]) for i in range(len(pngfiles))], outfile, indent=4)
