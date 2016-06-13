daf00b6322f111ab6315ae45c73cb4b835da59f9
root <mgckind@gmail.com>
2016-06-13 17:31:39 +0000

    updated api

--- app/api.py
+++ app/api.py
@@ -50,11 +50,13 @@ class ApiHandler(tornado.web.RequestHandler):
         response = { k: self.get_argument(k) for k in self.request.arguments }
         response2 = {'status':'error', 'message':''}
         response3={}
+        print response.keys()
         for k in response.keys():
             rs = response[k].replace('[','')
             k = k.lower()
             rs = rs.replace(']','')
             if k in float_input:
+                print k,rs
                 response2[k]=[float(i) for i in rs.split(',')]
             else:
                 response2[k] = rs
@@ -78,29 +80,43 @@ class ApiHandler(tornado.web.RequestHandler):
         if response2['status'] == 'ok':
             ra = response2['ra']
             dec = response2['dec']
-            xs = ''
-            ys = ''
-            listonly = ''
-            sendemail=''
-            cname = str(uuid.uuid4()) + '.csv'
-            df = pd.DataFrame(np.array([ra,dec]).T,columns=['RA','DEC'])
-            df.to_csv(newfolder+'/'+cname,sep=',',index=False)
-            del df
-            infP=infoP(user, passwd) 
-            now = datetime.datetime.now()
-            siid = cname[:-4]
-            tiid = user+'__'+cname[:-4]+'_{'+now.ctime()+'}'
-            if 'email' in response2:
-                sendemail = 'yes'
-                toemail = response2['email']
-            if sendemail == 'yes':
-                run=dtasks.desthumb.apply_async(args=[user_folder + cname, infP, user_folder+'results/'+siid+'/', xs,ys, siid, tiid, user_folder, listonly], task_id=tiid, link=dtasks.send_note.si(user, tiid, toemail))
-                response2['message'] = 'Job %s submitted. Email sent to %s on completion' % (siid, toemail) 
-                response3['job'] = siid 
-            else:
-                run=dtasks.desthumb.apply_async(args=[user_folder + cname, infP, user_folder+'results/'+siid+'/', xs,ys, siid ,tiid,user_folder, listonly], task_id=tiid)
-                response2['message'] = 'Job %s submitted.' % (siid)
-                response3['job'] = siid 
+            if len(ra) != len(dec):
+                response2['status']='error'
+                response2['message'] = 'RA and DEC arrays must have same dimensions'
+            xs = np.ones(len(ra))
+            ys = np.ones(len(ra))
+            if response2['status'] == 'ok':
+                if 'xsize' in response2:
+                    xs_read = response2['xsize']
+                    if len(xs) >= len(xs_read): xs[0:len(xs_read)] = xs_read
+                    else: xs = xs_read[0:len(xs)]
+                if 'ysize' in response2:
+                    ys_read = response2['ysize']
+                    if len(ys) >= len(ys_read): ys[0:len(ys_read)] = ys_read
+                    else: ys = ys_read[0:len(ys)]
+                listonly = ''
+                sendemail=''
+                cname = str(uuid.uuid4()) + '.csv'
+                df = pd.DataFrame(np.array([ra,dec,xs,ys]).T,columns=['RA','DEC','XSIZE','YSIZE'])
+                df.to_csv(newfolder+'/'+cname,sep=',',index=False)
+                del df
+                xs = ''
+                ys = ''
+                infP=infoP(user, passwd) 
+                now = datetime.datetime.now()
+                siid = cname[:-4]
+                tiid = user+'__'+cname[:-4]+'_{'+now.ctime()+'}'
+                if 'email' in response2:
+                    sendemail = 'yes'
+                    toemail = response2['email']
+                if sendemail == 'yes':
+                    run=dtasks.desthumb.apply_async(args=[user_folder + cname, infP, user_folder+'results/'+siid+'/', xs,ys, siid, tiid, user_folder, listonly], task_id=tiid, link=dtasks.send_note.si(user, tiid, toemail))
+                    response2['message'] = 'Job %s submitted. Email sent to %s on completion' % (siid, toemail) 
+                    response3['job'] = siid 
+                else:
+                    run=dtasks.desthumb.apply_async(args=[user_folder + cname, infP, user_folder+'results/'+siid+'/', xs,ys, siid ,tiid,user_folder, listonly], task_id=tiid)
+                    response2['message'] = 'Job %s submitted.' % (siid)
+                    response3['job'] = siid 
 
         response3['status']=response2['status']
         response3['message']=response2['message']
